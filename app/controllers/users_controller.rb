a5c270dc26e82cfac1e49b275d555c9dec7b7dbc
sei0816 <shtorishir0816@gmail.com>
2020-05-15 20:16:33 +0900

    add private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,10 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
