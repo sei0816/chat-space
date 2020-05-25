faeadce50e6bcbdebaed2ec1f00d7915a78b16c4
sei0816 <shtorishir0816@gmail.com>
2020-05-25 16:10:25 +0900

    define messages-instance

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :message do
+    content {Faker::Lorem.sentence}
+    image {File.open("#{Rails.root}/public/images/test_image.jpg")}
+    user
+    group
   end
   end
 end
