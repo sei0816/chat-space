39ff2c601af27e624e8350d3b7aa06a9fa4f6650
sei0816 <shtorishir0816@gmail.com>
2020-05-25 23:40:10 +0900

    define what to test
    in case ログインしていない場合

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -15,5 +15,11 @@ describe MessagesController do
       end
     end
 
+    context 'ログインしていない場合' do
+      it 'ログイン画面にリダイレクトすること' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
   end
  end
