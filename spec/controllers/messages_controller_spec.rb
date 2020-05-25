b33693b1b866fc20721fb4abb45671df0f9063d2
sei0816 <shtorishir0816@gmail.com>
2020-05-25 23:39:05 +0900

    define what to test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,5 +2,18 @@ require 'rails_helper'
 
 describe MessagesController do
   describe '#index' do
+
+    context 'ログインしている場合' do
+      it '@messageに正しい値が入っていること' do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+      it '@groupに正しい値が入っていること' do
+        expect(assigns(:group)).to eq group
+      end
+      it 'index.html.erbに遷移すること' do
+        expect(response).to render_template :index
+      end
+    end
+
   end
  end
