e82843e69754c9e3e4d2ffef82807ac77beec11b
sei0816 <shtorishir0816@gmail.com>
2020-05-26 18:48:35 +0900

    define test-content
    when user doesn't login, screen move to login-screen

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -55,6 +55,7 @@ describe MessagesController do
 
     context 'ログインしていない場合' do
       it 'ログイン画面にリダイレクトすること' do
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
