7abd551770f0248f6480eaa2827ddcde93084dcb
sei0816 <shtorishir0816@gmail.com>
2020-05-30 17:12:40 +0900

    set ajax function options

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,5 +3,13 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 });
