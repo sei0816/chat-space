3b09039e489d8c9d575fe30c1cef0ce95f92b366
sei0816 <shtorishir0816@gmail.com>
2020-05-30 18:58:13 +0900

    return code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,10 +6,15 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.create(message_params)
-    respond_to do |format|
-      format.html { redirect_to group_messages_path, notice: "メッセージを送信しました" }
-      format.json
+    @message = @group.messages.new(message_params)
+    if @message.save
+      respond_to do |format|
+        format.json
+      end
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
     end
   end
 
