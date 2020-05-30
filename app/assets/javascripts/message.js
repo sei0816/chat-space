7706f7c71eeae7e5145f4357f1ae6775c8c5e483
sei0816 <shtorishir0816@gmail.com>
2020-05-30 22:15:29 +0900

    define html

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,42 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image) {
+      var html = `<div class="message-list">
+                    <div class="message-header">
+                      <div class="message-header__curent-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="message-header__time-date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="talk">
+                      <p class="talk__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                    <img src=${message.image} >
+                  </div>`
+      return html;
+    } else {
+      var html = `<div class="message-list">
+                    <div class="message-header">
+                      <div class="message-header__curent-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="message-header__time-date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="talk">
+                      <p class="talk__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +48,9 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
     })
   })
 });
