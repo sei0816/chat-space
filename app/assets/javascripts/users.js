b7ed2e7bedf6b684b98495d2a340d8323f047403
sei0816 <shtorishir0816@gmail.com>
2020-06-03 21:43:00 +0900

    add done/fail-method

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -7,5 +7,11 @@ $(function() {
       data: { keyword: input },
       dataType: "json"
     })
+    .done(function(users) {
+      console.log("成功です");
+    })
+    .fail(function() {
+      console.log("失敗です");
+    });
   });
 });
